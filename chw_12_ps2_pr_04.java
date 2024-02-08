cefe67c791d2b4f42f0bc05923d910877836b6b3
atulsingh9936 <atul.singh2189@gmail.com>
2024-02-08 16:39:47 +0530

    practice_set_2_problem_03_04

---
+++ chw_12_ps2_pr_04.java
@@ -0,0 +1,20 @@
+package firstjavaproject;
+
+public class chw_12_ps2_pr_04 {
+
+    public static void main(String[] args) {
+
+        float v = 5;
+        float u= 3;
+        float a = 2;
+        float s = 4;
+
+        float x = (v*v)-(u*u);
+        float y= (2*a*s);
+        float z =x/y;
+        System.out.println(z);
+        System.out.println(7*49/7+35/7);
+
+
+    }
+}
