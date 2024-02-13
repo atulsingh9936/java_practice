506e5958ec720eacf9334eae1747aea0ba10a842
atulsingh9936 <atul.singh2189@gmail.com>
2024-02-13 23:35:32 +0530

    Java_strings

---
+++ chw_13_strings.java
@@ -0,0 +1,24 @@
+package firstjavaproject;
+
+import java.util.Scanner;
+
+public class chw_13_strings {
+    public static void main(String[] args) {
+      //  String name = new String("Atul");
+//        String name = "Harry";
+//        System.out.print("The name is :");
+//        System.out.print(name);
+
+//        int a =6;
+//        float b = 5.454f;
+//        System.out.printf("The value of a is %d and the value of b is %f",a,b);
+
+        Scanner sc = new Scanner(System.in);
+        String st = sc.nextLine();
+        System.out.println(st);
+
+
+
+
+   }
+}
---
+++ chw_14_string_methods.java
@@ -0,0 +1,39 @@
+package firstjavaproject;
+
+public class chw_14_string_methods {
+
+    public static void main(String[] args) {
+
+       String name ="AtulSingh";
+       String name2 ="Harry";
+//        System.out.println(name);
+//       int value = name.length();
+//        System.out.println(value);
+//        String lstring = name.toLowerCase();
+//        System.out.println(lstring);
+//        String ustring = name.toUpperCase();
+//        System.out.println(ustring);
+//
+//        String nonTrimmedString = "   Atul      ";
+//      String trimmedString =  nonTrimmedString.trim();
+//        System.out.println(trimmedString);
+//        System.out.println(name.substring(4));
+//        System.out.println(name.substring(1,4));
+//        System.out.println(name2.replace('r','p'));
+//        System.out.println(name.replace( "tul","nuj"));
+//        System.out.println(name.startsWith("Atu"));
+//        System.out.println(name.endsWith("ngh"));
+//        System.out.println(name.charAt(2));
+//        String modifiedString = "Harryrryrry";
+//        System.out.println(modifiedString.indexOf("yrry"));
+//        System.out.println(modifiedString.indexOf("rry",4));
+//        System.out.println(modifiedString.lastIndexOf("rry"));
+//        System.out.println(modifiedString.lastIndexOf("rry",7));
+        System.out.println(name.equals("AtulSingh"));
+        System.out.println(name.equalsIgnoreCase("atulsinGH"));
+
+        System.out.println("I am escape sequence\" double quote");
+
+
+    }
+}
