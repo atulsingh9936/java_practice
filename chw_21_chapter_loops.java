4c04c155d52c91a43c6040f9e77aebf60b59261b
atulsingh9936 <atul.singh2189@gmail.com>
2024-02-17 23:25:52 +0530

    while and do while loop

---
+++ chw_21_chapter_loops.java
@@ -0,0 +1,27 @@
+package firstjavaproject;
+
+public class chw_21_chapter_loops {
+    public static void main(String[] args) {
+
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println("Using while loops");
+//        int i=1;
+//        while(i<=3){
+//            System.out.println(i);
+//            i++;
+//        }
+//        System.out.println("Finished running while loop");
+
+//        while(true){
+//            System.out.println("i am infinite while loop");
+//        }
+
+        int i=100;
+        while(i<=200){
+            System.out.println(i);
+            i++;
+            }
+    }
+}
