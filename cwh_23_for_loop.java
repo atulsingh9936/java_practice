582c0c1b48f6bc7d18e3b83e73bdc8f33b0dcc55
atulsingh9936 <atul.singh2189@gmail.com>
2024-02-18 21:29:11 +0530

    for loop

---
+++ cwh_23_for_loop.java
@@ -0,0 +1,31 @@
+package firstjavaproject;
+
+public class cwh_23_for_loop {
+
+    public static void main(String[] args) {
+
+//        for (int i=1;i<=10;i++){
+//            System.out.println(i);
+//        }
+
+        //2i = even number= 0,2,4,6,8,10
+        //2i+1 =odd number =1,3,5,7,9,11
+
+//             int n=1000;
+//             for (int i=0;i<n;i++)
+//                 System.out.println(2*i+1);
+        //Decrement operator
+//        for(int i=5;i>0;i--){
+//            System.out.println(i);
+//
+//        }
+//        for (int i = 7; i != 0; i--) {
+//            System.out.println(i);
+//
+//       }
+        int n=0;
+        for (int i = 10; i > n; i--) {
+            System.out.println(i);
+        }
+    }
+}
